25b609e6ed27a737af9a8a40d333e1334e667e38
Elvandro <elvisamuni22@gmail.com>
2019-08-05 12:20:07 +0300

    Modified the project class

--- src/app/fibre/models/project.ts
+++ src/app/fibre/models/project.ts
@@ -1,2 +1,11 @@
 export class Project {
+    'id': number;
+    'project_name': any;
+    'site_name': [];
+    'start_date': any;
+    'end_date': any;
+    'created_by': number;
+    'created_at': any;
+    'updated_at': any;
+    'is_active': boolean;
 }
