f9d4e9a3d8bc8b51777de2a624c0b27e7837049f
Elvandro <elvisamuni22@gmail.com>
2019-08-05 12:16:16 +0300

    Created the site model

---
+++ src/app/bts/models/site.ts
@@ -0,0 +1,20 @@
+export class Site {
+    'id': number;
+    'turn_around_time': number;
+    'progress': number;
+    'site_name': any;
+    'site_number': any;
+    'BTS_type': any;
+    'site_owner': any;
+    'geotech_file': any;
+    'access_letter': any;
+    'approved_drawing': any;
+    'final_acceptance_cert': any;
+    'final_acceptance_cert_comment': any;
+    'created_at': any;
+    'updated_at': any;
+    'is_active': boolean;
+    'icon': any;
+    'location': number;
+    'created_by': number;
+}
