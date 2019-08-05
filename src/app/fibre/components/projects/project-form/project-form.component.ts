0db909f5a280b582f471d7c9805ce6e00b7aeeb7
Elvandro <elvisamuni22@gmail.com>
2019-08-05 12:23:19 +0300

    Added the logic to create a new project

--- src/app/fibre/components/projects/project-form/project-form.component.ts
+++ src/app/fibre/components/projects/project-form/project-form.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
+
+import { ProjectService } from '../../../services/project.service';
+import { SiteService } from 'src/app/bts/services/site.service';
+import { Site } from 'src/app/bts/models/site';
+
 
 @Component({
   selector: 'app-project-form',
@@ -7,9 +15,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectFormComponent implements OnInit {
 
-  constructor() { }
+  site$: Site[];
+  projectForm: FormGroup;
+  userId = sessionStorage.getItem('id');
+
+
+  constructor(
+    private fb: FormBuilder,
+    private siteService: SiteService,
+    private projectService: ProjectService,
+    private router: Router,
+    private datePipe: DatePipe
+  ) { }
 
   ngOnInit() {
+    this.projectForm = this.fb.group({
+      project_name: ['', Validators.required],
+      site_name: ['', Validators.required],
+      start_date: ['', Validators.required]
+    });
+    this.getSites();
+  }
+
+  getSites() {
+    this.siteService.fetchSites()
+    .subscribe(data => {
+      this.site$ = data;
+    });
+  }
+
+
+  saveProject() {
+    const formData = new FormData();
+
+    const date = this.projectForm.get('start_date').value;
+    const myDate = this.datePipe.transform(date, 'yyyy-MM-dd');
+
+    formData.append('project_name', this.projectForm.get('project_name').value);
+    formData.append('site_name', this.projectForm.get('site_name').value);
+    formData.append('start_date', myDate);
+    formData.append('created_by', this.userId);
+
+    this.projectService.createProject(formData)
+    .subscribe(
+      () => {
+        window.alert('Successfully created a project');
+        this.router.navigate(['/fibre/']);
+        console.log('Success!');
+      },
+      error => {
+        window.alert('An Error occured. Please check the form and correct the Details');
+        console.error('Error!', error);
+      });
   }
 
 }
