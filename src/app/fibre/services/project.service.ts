a31bd42c31359bf94671e40ae7fa682c974ce933
Elvandro <elvisamuni22@gmail.com>
2019-08-05 12:19:43 +0300

    Created the project service

--- src/app/fibre/services/project.service.ts
+++ src/app/fibre/services/project.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Project } from '../models/project';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
-  constructor() { }
+  apiUrl = 'http://68.183.79.24/erp_fiber_ftts/fttsproject/';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  fetchProjects() {
+    return this.http.get<Project[]>(this.apiUrl);
+  }
+
+  createProject(formData) {
+    return this.http.post(this.apiUrl, formData);
+  }
 }
